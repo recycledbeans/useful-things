17ca402740338c3c0b12fdfdc4b9dc015e23df9a
recycledbeans <matt@recycledbeans.com>
2020-10-25 15:45:35 +0000

    Fix file name to comply with PSR-4 (cs fix)

--- src/Helpers/Date.php
+++ src/Helpers/Date.php
@@ -5,32 +5,35 @@ namespace RecycledBeans\UsefulThings\Helpers;
 class Date
 {
     /**
-     * Returns list of years in the future
-     * @param int $number
+     * Returns list of years in the future.
+     *
+     * @param int  $number
      * @param bool $include_today
+     *
      * @return array
      */
     public static function futureYears($number = 5, $include_today = true)
     {
         $years = [];
 
-        for($i = $include_today ? 0 : 1; $i <= $number; $i++) {
-            $years[date("Y", strtotime("+ $i years"))] = date("Y", strtotime("+ $i years"));
+        for ($i = $include_today ? 0 : 1; $i <= $number; ++$i) {
+            $years[date('Y', strtotime("+ $i years"))] = date('Y', strtotime("+ $i years"));
         }
 
         return $years;
     }
 
     /**
-     * Returns months of the year
+     * Returns months of the year.
+     *
      * @return array
      */
     public static function months()
     {
         $months = [];
 
-        for ($i = 0; $i < 12; $i++) {
-            $months[date("m", strtotime("+ $i months"))] = date("F", strtotime("+ $i months"));
+        for ($i = 0; $i < 12; ++$i) {
+            $months[date('m', strtotime("+ $i months"))] = date('F', strtotime("+ $i months"));
         }
 
         return $months;
